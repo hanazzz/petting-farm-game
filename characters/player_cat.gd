f08f70d65bc684efdfef69c19141a5d407629a4b
Hana Zait <hana.o.zait@gmail.com>
2025-10-20 00:56:04 -0700

    Add logic for player animations (idle + walk)

--- characters/player_cat.gd
+++ characters/player_cat.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 @export var starting_direction = Vector2(0, 1)
 
 @onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
-	animation_tree.set("parameters/Idle/blend_position", starting_direction)
+	# Set player's initial direction
+	update_animation_paramters(starting_direction)
 
 func _physics_process(delta: float) -> void:
 	# Get input direction
@@ -14,10 +16,30 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	).normalized()
-
 	# TODO: Replace above with Input.get_vector()? Look into documentation.
+	
+	# Update animation
+	update_animation_paramters(input_direction)
 	
 	#Update velocity
 	velocity = input_direction * move_speed
 	
 	move_and_slide()
+	
+	pick_new_state()
+
+
+func update_animation_paramters(move_input : Vector2):
+	# Don't update animation if there is no input
+	if (move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+
+
+# Choose state based on player movement
+func pick_new_state():
+	if (velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
+		
