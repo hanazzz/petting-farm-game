7153849d774585718922b212a9ae634ff2a9ea76
Hana Zait <hana.o.zait@gmail.com>
2025-10-23 03:32:22 -0700

    Refactor code related to door animation
    Put door and its script in one scene (on a StaticBody2D with an AnimatedSprite2D instead of using multiple tile map layers)

--- objects/door.gd
+++ objects/door.gd
@@ -1,12 +1,12 @@
-extends TileMapLayer
+extends StaticBody2D
 
 @onready var interactable: Area2D = $Interactable
-# Get open door map layer
-@onready var house_door_open: TileMapLayer = $"../HouseDoorOpen"
-# Get roof map layer
-@onready var house_roof: TileMapLayer = $"../HouseRoof"
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var house_roof: TileMapLayer = %HouseRoof
+@onready var closed_collision_shape_2d: CollisionShape2D = $ClosedCollisionShape2D
 
 var is_door_closed : bool = true
+
 
 func _ready() -> void:
 	# Set door's interaction function
@@ -17,32 +17,31 @@ func _ready() -> void:
 	else:
 		$Interactable.interact_name = "Close door"
 
+
 func _on_interact():
 	# If door is closed, open it
 	if is_door_closed == true:
-		# Hide closed door map layer
-		hide()
-		# Show open door map layer
-		house_door_open.show()
-		# Disable collisions on closed door map layer
-		collision_enabled = false
 		# Hide roof
 		house_roof.hide()
+		# Play opening animation
+		$AnimatedSprite2D.play("opening")
+		# Disable collision layer
+		$ClosedCollisionShape2D.set_disabled(true)
+		print($ClosedCollisionShape2D.disabled)
 		# Update door state
 		is_door_closed = false
 		# Update door interaction name
 		$Interactable.interact_name = "Close door"
 	# If door is open, close it
 	else:
-		# Show closed door map layer
-		show()
-		# Hide open door map layer
-		house_door_open.hide()
-		# Enable collisions on closed door map layer
-		collision_enabled = true
-		# Show roof
-		house_roof.show()
+		# Play closing animation
+		$AnimatedSprite2D.play("closing")
+		# Enable collision layer
+		$ClosedCollisionShape2D.set_disabled(false)
+		print($ClosedCollisionShape2D.disabled)
 		# Update door state
 		is_door_closed = true
+		# Show roof
+		house_roof.show()
 		# Update door interaction name
 		$Interactable.interact_name = "Open door"
