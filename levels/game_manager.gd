40af02f6847ace716954d38e89ebd4e97b0bf750
Hana Zait <hana.o.zait@gmail.com>
2025-10-24 20:03:17 -0700

    Add game timer
    Also add additional interaction key

--- levels/game_manager.gd
+++ levels/game_manager.gd
@@ -2,15 +2,24 @@ extends Node
 
 # Access points label
 @onready var points_label: Label = %PointsLabel
+# Access timer
+@onready var timer: Timer = %Timer
+# Access timer label
+@onready var timer_label: Label = %TimerLabel
 
 # Start player with zero points
 var player_points : int = 0
 
 
+func _process(delta: float) -> void:
+	# Get amount of time left and format it
+	var time_left = str(int(%Timer.time_left)).pad_zeros(2)
+	# Update timer label
+	%TimerLabel.text = "Time left: " + str(time_left)
+
 # Increase player points
 func add_points(points: int):
 	# Update points
 	player_points += points
-	print(player_points)
 	# Update points label
 	%PointsLabel.text = "Points: " + str(player_points)
